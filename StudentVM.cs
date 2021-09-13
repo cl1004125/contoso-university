e69928c9d07454a0e26e6e44da588628581e54f7
cl1004125 <77294178+cl1004125@users.noreply.github.com>
2021-09-12 21:30:41 -0500

    Add files via upload

---
+++ StudentVM.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace ContosoUniversity.ViewModels
+{
+    #region snippet
+    public class StudentVM
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+    }
+    #endregion
+}
